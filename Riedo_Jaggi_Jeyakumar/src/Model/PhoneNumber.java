ce266916a28d231e343deeb1e186ab7adcbe435c
nooone <nooone@compute>
2017-05-12 15:28:36 +0200

    ajout variables + accesseurs

--- Riedo_Jaggi_Jeyakumar/src/Model/PhoneNumber.java
+++ Riedo_Jaggi_Jeyakumar/src/Model/PhoneNumber.java
@@ -1,5 +1,27 @@
 package Model;
 
 public class PhoneNumber {
-
+	
+	private String typePhoneNumber[] = {"mobile","privé","bureau"};
+	private String phoneNumber;
+	
+	public PhoneNumber(String []typePhoneNumber, String PhoneNumber){
+		this.typePhoneNumber = typePhoneNumber;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+	public String[] getTypePhoneNumber() {
+		return typePhoneNumber;
+	}
+	public void setTypePhoneNumber(String[] typePhoneNumber) {
+		this.typePhoneNumber = typePhoneNumber;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		phoneNumber = phoneNumber;
+	}
+		
 }
