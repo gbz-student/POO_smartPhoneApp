dcd318425047d9b9eb92f71230c3becf54a678bf
Ken Jäggi <jaggi@icare.ch>
2017-05-12 14:56:26 +0200

    Commit de la première structure de base

---
+++ Riedo_Jaggi_Jeyakumar/src/Launcher/MainJFrame.java
@@ -0,0 +1,12 @@
+package Launcher;
+
+import javax.swing.JFrame;
+
+public class MainJFrame extends JFrame{
+
+	public MainJFrame() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	}
+
+}
---
+++ Riedo_Jaggi_Jeyakumar/src/Main.java
@@ -0,0 +1,13 @@
+import Launcher.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MainJFrame mainFrame = new MainJFrame();
+		
+		mainFrame.setVisible(true);
+
+	}
+
+}
