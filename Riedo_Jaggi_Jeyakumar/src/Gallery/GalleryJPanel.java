e22b46bd26cca3248c9f528395b302d0b586937a
nooone <nooone@compute>
2017-05-19 09:31:42 +0200

    nettoyage initial de la classe

--- Riedo_Jaggi_Jeyakumar/src/Gallery/GalleryJPanel.java
+++ Riedo_Jaggi_Jeyakumar/src/Gallery/GalleryJPanel.java
@@ -7,18 +7,9 @@ import javax.swing.JPanel;
 import Launcher.MainJFrame;
 
 public class GalleryJPanel extends JPanel{
-	private JButton buttonBack = new JButton("Retour");
 		
 	public GalleryJPanel(){
-		add(buttonBack);
 		
-		buttonBack.addActionListener(new ListerButtonBack());
 	}
-	
-	class ListerButtonBack implements ActionListener{
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			MainJFrame.changePanel("launcherJPanel");	
-		}
-	}
+
 }
