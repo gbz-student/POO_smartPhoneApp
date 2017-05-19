2722e573a2df45138d03c7cebf2cec3b4a31e64d
Ken Jäggi <ken.jaggi@students.hevs.ch>
2017-05-19 09:59:32 +0200

    Ajout d’un bouton menu qui retour sur le launcher

--- Riedo_Jaggi_Jeyakumar/src/Launcher/MainJFrame.java
+++ Riedo_Jaggi_Jeyakumar/src/Launcher/MainJFrame.java
@@ -1,18 +1,20 @@
 package Launcher;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-
 import Caculate.CalculateJPanel;
 import Contact.ContactJPanel;
 import Gallery.GalleryJPanel;
 
 public class MainJFrame extends JFrame{
-	
 	private JButton buttonMenu = new JButton("Menu");
+	
 	private static CardLayout cardLayout;
 	public static JPanel cards;
-	private JPanel launcherPanel;
+	private JPanel launcherPanel= new LauncherJPanel();
 	private JPanel contactJPanel = new ContactJPanel();
 	private JPanel calculateJPanel = new CalculateJPanel();
 	private JPanel galleryJPanel = new GalleryJPanel();
@@ -21,19 +23,28 @@ public class MainJFrame extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(320, 480);
 		
-		launcherPanel = new LauncherJPanel();
-		
 		cards = new JPanel (cardLayout = new CardLayout());
 		cards.add(launcherPanel, "launcherJPanel");
 		cards.add(contactJPanel, "contactJPanel");
 		cards.add(calculateJPanel, "calculateJPanel");
 		cards.add(galleryJPanel, "galleryJPanel");
 		
+		//setContentPane(cards);
+		add(cards, BorderLayout.NORTH);
 		
-		setContentPane(cards);
+		add(buttonMenu, BorderLayout.SOUTH);
+		
+		buttonMenu.addActionListener(new ListerButtonMenu());
 	}
 	
 	public static void changePanel(String name){
 		cardLayout.show(cards, name);
     }
+	
+	class ListerButtonMenu implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			MainJFrame.changePanel("launcherJPanel");
+		}
+	}
 }
