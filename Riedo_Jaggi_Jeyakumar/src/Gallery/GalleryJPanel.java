1706c4a6a034f399585ba6a46ae8a711a181b31e
Gabriel <gabriel.riedo@students.hevs.ch>
2017-05-30 13:33:42 +0200

    affichage des images et scroll ok
    erreurs dans la résolution du type Image nécessitant de transtyper vers
    java.awt.Image dans Image.java

--- Riedo_Jaggi_Jeyakumar/src/Gallery/GalleryJPanel.java
+++ Riedo_Jaggi_Jeyakumar/src/Gallery/GalleryJPanel.java
@@ -2,47 +2,59 @@ package Gallery;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.sound.midi.MidiDevice.Info;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 import Launcher.MainJFrame;
 import MainPackage.TitleJPanel;
 
-public class GalleryJPanel extends JPanel{
+public class GalleryJPanel extends JPanel {
 		
-		private JPanel galleryPanel = new JPanel();
+//		private JPanel galleryPanel = new JPanel();
+		private TitleJPanel title = new TitleJPanel("Mes images");
+		private Image images = new Image();
+
+		
 		 
 		
 		
-		public GalleryJPanel(){
+		public GalleryJPanel() {
 			
 			this.setVisible(true);
 			this.setLayout(new BorderLayout());
 			
-			TitleJPanel title = new TitleJPanel("Mes images");
+			JPanel imagesPanel = images.generateImages();
+			GridLayout column = new GridLayout(0, 3);
+			column.setVgap(20);
+			imagesPanel.setLayout(column);
+						
+			JScrollPane imgScroll = new JScrollPane(imagesPanel);
+			imgScroll.setPreferredSize(new Dimension(400, 400));
+			
 			add(title, BorderLayout.NORTH);
+			add(imgScroll, BorderLayout.CENTER);
 			
-			
-			Image images = new Image();
-			
-			ArrayList<String>listImage = new ArrayList<String>(Arrays.asList("pet1.jpg","pet2.jpg","pet3.jpg","pet4.jpg","pet5.jpg","pet6.jpg","pet7.jpg"));
-
-			images.createButton(listImage);
-			
-//			add(images, BorderLayout.CENTER);
-			
-		
-
+					
 			
 		}
+		
+
 		
 		class ListerButtonBack implements ActionListener{
 			@Override
--- Riedo_Jaggi_Jeyakumar/src/Gallery/Image.java
+++ Riedo_Jaggi_Jeyakumar/src/Gallery/Image.java
@@ -1,65 +1,92 @@
 package Gallery;
 
-import java.util.ArrayList;
-
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.File;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Image extends JFrame{
-
-//	ArrayList<String> imgList = new ArrayList<String>();
-
-//	public void addImage(String str){
-//		imgList.add(str);
-//		
-//	}
-		
-//	public Image(String location){
-//		JLabel img = new JLabel(new ImageIcon(location));
-//		
-//		
-//	}
+public class Image extends JPanel{
+	
+	private static double ImgSizeConstraint = 120;
+	
+	public Image() {
+	
+	}
 	
 
-	public void createButton(ArrayList<String>buttonList){
-		int i;
-		for(i=0;i<buttonList.size();i++){
-			String str = buttonList.get(i);
-			JButton button = new JButton(str);
-			
-			button.setSize(200,200);
+	private File getImagesPath(){
+		File projectPath = new File(".");
+		String projectAbsolutePath = projectPath.getAbsolutePath();
+		File imagesFolder = new File(projectAbsolutePath+"/img_library");
+		return imagesFolder;
+	}
+	
+	
+	private String[]getImagesList(File Folder){
+		String[]imagesList = Folder.list();
+		return imagesList;
+	}
+
+	
+	public JPanel generateImages(){
+		JPanel imagesPanel= new JPanel();
+		String[]imagesList= getImagesList(getImagesPath());
+		
+		for (int i = 0; i < imagesList.length; i++) {
+			String path = getImagesPath()+"/"+imagesList[i];
+			ImageIcon imgResized = imgResize(path);
+
+			imagesPanel.add(new JLabel(imgResized));
+//			imagesPanel.add(new JLabel(getImagesPath()+"/"+imagesList[i]));
 
 		}
 		
+		return imagesPanel;
+		
+	}
+	//Opération de redimensionnement
+	private ImageIcon imgResize(String path){
+//		ImageIcon img = new ImageIcon(path);
+//		Image image = img.getImage();
+//		setImgBoudaries(image);
+		
+		java.awt.Image image = Toolkit.getDefaultToolkit().getImage(path);
+		setImgBoudaries(image);
+		
+		Image newing = image.getScaledInstance(width, height, hints);
+		ImageIcon imgResized = new ImageIcon(newing);
+		
+		return imgResized;
 	}
 	
+	//Crop de l'image
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	private int calculateImageHeight(int width){
-		int height;
+	//Définit la dimension de l'image proportionnellement
+	private Dimension setImgBoudaries(java.awt.Image image){
+		Dimension imgBoudaries = image.getSize();
+		double width = imgBoudaries.getWidth();
+		double height = imgBoudaries.getHeight();
 		
-		height = width/4 * 3;
-		
-		return height;
+		if(width>height){
+			width = setImgReduction(width, height);
+			imgBoudaries.setSize(width, ImgSizeConstraint);
+		}
+		else{
+			height = setImgReduction(height, width);
+			imgBoudaries.setSize(ImgSizeConstraint, height);
+		}
+		return imgBoudaries;
 	}
 	
-	public void setImageSize(int imgWidth){
-		int imageHeight;
-		
-		imageHeight = calculateImageHeight(imgWidth);
-		
-		this.setSize(imgWidth,imageHeight);
+	//Calcul de réduction
+	private double setImgReduction(double bigger, double smaller){
+		bigger = (bigger/smaller)*ImgSizeConstraint;
+		return bigger;
 	}
+		
+	public void addImage(){
 
-
-	
+	}
 }
