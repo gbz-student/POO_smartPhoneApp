21db93a61b47c569711afe0931724cad8c45def9
ashan <ashan@DESKTOP-EF1R0VF>
2017-06-15 13:26:16 +0200

    librairie JUnit4 pour Ken

--- Riedo_Jaggi_Jeyakumar/src/Caculate/ActionsScreen.java
+++ Riedo_Jaggi_Jeyakumar/src/Caculate/ActionsScreen.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -19,8 +18,10 @@ public class ActionsScreen extends JPanel {
 	// definir la taille et la police du champ de texte
 	private Font tp = new Font("Arial",Font.PLAIN,80);
 	
-	
-	
+
+	/**
+	 * constructeur permettant de personaliser le JLabel
+	 */
 	public ActionsScreen(){
 		
 		this.setLayout(new BorderLayout());
@@ -35,13 +36,13 @@ public class ActionsScreen extends JPanel {
 		// alignement des chiffres à droit dans le JLabel
 		text.setHorizontalAlignment(JLabel.RIGHT);
 		
-	    // ajout du champ de texte dans le Jpanel
+	    // ajout du JLabel dans le Jpanel
 		this.add(text);
 				
-
 		
 	}
-
+	
+	
 	
 
 	
--- Riedo_Jaggi_Jeyakumar/src/Caculate/CalculateJPanel.java
+++ Riedo_Jaggi_Jeyakumar/src/Caculate/CalculateJPanel.java
@@ -1,18 +1,10 @@
 package Caculate;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+
 
 import Launcher.MainJFrame;
 
@@ -22,7 +14,9 @@ public class CalculateJPanel extends JPanel{
 	
 	private ActionsButton butpanel = new ActionsButton();
 	
-	
+	/**
+	 * constructeur permettant d'afficher la calculatrice
+	 */
 	public CalculateJPanel(){
 		
 		this.setVisible(true);
